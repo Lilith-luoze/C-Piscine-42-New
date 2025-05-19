3cba1e6f512eeebcfbb9552f90cb3d4c4332d92e
Luoze Guo <luozguo@1-C-1.42heilbronn.de>
2025-05-19 17:12:37 +0200

    do 4,5

--- note_c04/ex04/ft_putnbr_base.c
+++ note_c04/ex04/ft_putnbr_base.c
@@ -1,19 +1,48 @@
-# include <stdio.h>
-# include <string.h>
-# include <unistd.h>
-# include <limits.h>
 
-void ft_putnbr_base(int nbr, char *base)
-{
+// # include <unistd.h>
+
+// void    ft_put_bi(int nbr)
+// {
+//     char *bi = "01";
+//     if (nbr >= 2)
+//         return (ft_put_bi(nbr / 2));
+//     nbr = nbr % 2;
+//     write(1, bi + nbr , 1);
+// }
+
+// //do i use parameter or my local var?
+// void ft_putnbr_base(int nbr, char *base)
+// {
+//     //check if *base is valid, and verify the system 
+//     if 
+//     while (*base)
+//     {
+//         if (*base == '+' || *base == '-'
+//         {
+//             return;
+//         })
+
+//     }
+//     if (*base)
+//     //function; display number : i to a
+//     //deal with minus first here
+//     if (nb < 0)
+//     ft_put_dec(nbr);
+//     ft_put_bi(nbr);
+//     ft_put_hex(nbr);
+//     ft_put_octal(nbr);
+
+// }
+
+
+// # include <stdio.h>
+// # include <string.h>
+// # include <limits.h>
+
+// int main()
+// {
+//     char *str = "  ---+--+1234ab567";
     
-}
-
-
-
-int main()
-{
-    char *str = "  ---+--+1234ab567";
-    
-    printf("%i", ft_atoi(str));
-    return (0);
-}
+//     printf("%i", ft_atoi(str));
+//     return (0);
+// }
--- note_c04/ex05/ft_atoi_base.c
+++ note_c04/ex05/ft_atoi_base.c
@@ -1,13 +1,14 @@
-# include <stdio.h>
-# include <string.h>
-# include <unistd.h>
-# include <limits.h>
+// # include <unistd.h>
+// int ft_atoi_base(char *str, char *base)
+// {
+//     //edge case
+//     if (base == NULL || *base == '1' ) // empty or "size of 1"
+//         return (0);
+//     if (*)
+//     if ()
+// }
 
-int ft_atoi_base(char *str, char *base)
-{
-    //edge case
-    if (base == NULL || *base == '1' ) // empty or "size of 1"
-        return (0);
-    if (*)
-    if ()
-}
+// # include <stdio.h>
+// # include <string.h>
+
+// # include <limits.h>
