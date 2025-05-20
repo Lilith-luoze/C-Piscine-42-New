2f85c6256d389173b651391719097f246077dc04
laptop <xiaoshui@laptop.com>
2025-05-21 00:51:55 +0200

    update

--- note_c04/ex04/ft_putnbr_base.c
+++ note_c04/ex04/ft_putnbr_base.c
@@ -27,7 +27,7 @@ void ft_prt_char(char *prt)
     }
 }
 
-//prt[33]
+//prt[34]
 void  ft_itoa_base(int nb , char * avail_digits , char *prt)
 {
     unsigned int redix;
@@ -35,9 +35,9 @@ void  ft_itoa_base(int nb , char * avail_digits , char *prt)
     int i;
     i = -1;
     redix = (unsigned int)ft_strlen(avail_digits);
-    while (++i < 33)
+    while (++i < 34)
         prt[i] = '+';
-    prt[32] = '\0';
+    prt[33] = '\0';
     if (nb < 0)
     {
         prt[0] = '-'; 
@@ -45,14 +45,14 @@ void  ft_itoa_base(int nb , char * avail_digits , char *prt)
     }
     else
         prt_nb = (unsigned int)nb ;
-    i = 0;
+    i = 1;
     while (prt_nb >= redix)
     {   
-        prt[31 - i] = avail_digits[prt_nb % redix];
+        prt[33 - i] = avail_digits[prt_nb % redix];
         prt_nb /= redix;
         i++;
     }
-    prt[31 - i] = avail_digits[prt_nb % redix];
+    prt[33 - i] = avail_digits[prt_nb % redix];
 }
 
 int	ft_existdup(char *str)
@@ -84,50 +84,55 @@ int	ft_existdup(char *str)
 void ft_putnbr_base(int nbr, char *base)
 {
     int len;
-    char prt[33];
+    char prt[34];
     len = 0;
     if (base == NULL)
         return;
     while (base[len])
     {
-        if (base[len] == '+' || base[len] == '-' || base[len] == ' ')
+        if (base[len] == '+' || base[len] == '-' )
+            return;
+        if (base[len] <= 31 || base[len] >= 127)
             return;
         len++;
     }
-    if (len <= 1 || base[0] == ' ' || ft_existdup(base))
+    if (len <= 1 || ft_existdup(base))
         return;
     ft_itoa_base(nbr , base ,prt);
     ft_prt_char(prt);
 }
 
-// # include <stdio.h>
-// # include <string.h>
-// # include <limits.h>
+# include <stdio.h>
+# include <string.h>
+# include <limits.h>
 
-// int	main(void)
-// {
-// 	ft_putnbr_base(42, "0123456789");
-// 	printf("  --> Expected: 42\n");
+int	main(void)
+{
+	ft_putnbr_base(42, "0123456789");
+	printf("  --> Expected: 42\n");
 
-// 	ft_putnbr_base(0, "01");
-// 	printf("  --> Expected: 0\n");
+	ft_putnbr_base(0, "01");
+	printf("  --> Expected: 0\n");
 
-// 	ft_putnbr_base(42, "0123456789ABCDEF");
-// 	printf("  --> Expected: 2A\n");
+	ft_putnbr_base(INT_MIN, "01");
+	printf("  --> Expected: -10..0000(*31)\n");
 
-// 	ft_putnbr_base(-42, "0123456789");
-// 	printf("  --> Expected: -42\n");
+	ft_putnbr_base(42, "0123456789ABCDEF");
+	printf("  --> Expected: 2A\n");
 
-// 	ft_putnbr_base(42, "01234");
-// 	printf("  --> Expected: 132\n");
+	ft_putnbr_base(-42, "0123456789");
+	printf("  --> Expected: -42\n");
 
-// 	ft_putnbr_base(42, "012344");
-// 	printf("  --> Expected: (nothing)\n");
+	ft_putnbr_base(42, "01234");
+	printf("  --> Expected: 132\n");
+
+	ft_putnbr_base(42, "012344");
+	printf("  --> Expected: (nothing)\n");
 
 
-// 	ft_putnbr_base(42, "0");
-// 	printf("  --> Expected: (nothing)\n");
+	ft_putnbr_base(42, "0");
+	printf("  --> Expected: (nothing)\n");
 
-// 	return (0);
-// }
+	return (0);
+}
 
